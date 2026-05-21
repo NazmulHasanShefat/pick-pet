1b49786fd07f5b31e84226a1dedfd0348ffec708
NazmulHasanShefat <mdnazmulhasan0879@gmail.com>
2026-05-22 02:02:56 +0600

    added badge in listing card

--- src/app/profile-dashbord/my-listings/MyListingCard.jsx
+++ src/app/profile-dashbord/my-listings/MyListingCard.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { CheckImageUrl } from "@/context/functions";
 import Image from "next/image";
 import { use } from "react";
@@ -9,28 +9,35 @@ import Link from "next/link";
 import { authClient } from "@/lib/auth-client";
 import { ListingView } from "./ListingView";
 import { RequestModal } from "./RequestModal";
-
+import { Chip } from "@heroui/react";
 
 export default function MyListCart({ myListPromise }) {
-  const {data:session} = authClient.useSession();
+  const { data: session } = authClient.useSession();
   const myList = use(myListPromise);
-  
+
   const ListInfo = myList?.data;
-  console.log(ListInfo.length)
+  console.log(ListInfo.length);
 
   return ListInfo.length === 0 || !ListInfo ? (
-      <div className="col-span-4 flex text-center">
+    <div className="col-span-4 flex text-center">
       <h1>Data not found</h1>
-    </div>   
+    </div>
   ) : (
-     ListInfo.map((list, index) => {
+    ListInfo.map((list, index) => {
       const isValid = CheckImageUrl(list?.PetImageUrl);
-      if(list?.Owner_Email === session?.user?.email){
+      if (list?.Owner_Email === session?.user?.email) {
         return (
           <div
             key={list._id}
-            className="border border-emerald-500 rounded-3xl hover:translate-y-2 transition-translate duration-200"
+            className="border relative border-emerald-500 rounded-3xl hover:translate-y-2 transition-translate duration-200"
           >
+            {list?.adoptedStatus === true ? (
+              <Chip color="warning" className="absolute left-0 top-0 ml-2 mt-2">
+                Adopted
+              </Chip>
+            ) : (
+              ""
+            )}
             <Image
               src={isValid ? list?.PetImageUrl : "/tommy.jpg"}
               className="w-full h-[200px] rounded-b-2xl rounded-t-3xl object-cover"
@@ -43,7 +50,8 @@ export default function MyListCart({ myListPromise }) {
               <p className="text-md mt-1 line-clamp-2"> {list?.description} </p>
               <p className="mt-1 text-base text-gray-400">Dhaka, bangladesh</p>
               <p className="mt-1 text-base text-gray-400">
-                Adation fee: <span className="text-emerald-400"> {list?.adoptionFee}tk</span>
+                Adation fee:{" "}
+                <span className="text-emerald-400"> {list?.adoptionFee}tk</span>
               </p>
               <div className="flex items-center flex-col gap-5 mt-3">
                 <div className="flex justify-between items-center w-full">
@@ -56,14 +64,14 @@ export default function MyListCart({ myListPromise }) {
                   </Link>
                   <DeleteButton deleteId={list?._id} />
                 </div>
-  
+
                 <div className="flex justify-between items-center w-full">
-                  <ListingView currentDetails={list}/>
+                  <ListingView currentDetails={list} />
                   {/* <button className="px-2 flex items-center gap-2 py-1 text-xs rounded-lg text-white bg-transparent border border-emerald-500 cursor-pointer">
                     <MdOutlineRequestPage />
                     Requests
                   </button> */}
-                  <RequestModal currentDetails={list}/>
+                  <RequestModal currentDetails={list} />
                 </div>
               </div>
             </div>
