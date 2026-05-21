6b332bebb0d8826c63f3631a66fd6fb58b8a7d56
NazmulHasanShefat <mdnazmulhasan0879@gmail.com>
2026-05-21 17:20:22 +0600

    fix profile dashbord heading spacing

--- src/app/profile-dashbord/add-pet/page.jsx
+++ src/app/profile-dashbord/add-pet/page.jsx
@@ -3,8 +3,10 @@ import AddPetForm from "./AddPetForm";
 export default function page() {
   return (
     <section className="py-10">
+      <div className="px-5">
       <h1 className="text-2xl">Add A New Pet</h1>
       <p className="text-gray-500">Carefully add you pet details</p>
+      </div>
       <div className="w-full px-5 flex items-center justify-center">
         <AddPetForm />
       </div>
