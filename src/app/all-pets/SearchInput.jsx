91f9b26d4fd062fe832086e12fb27fddc33a95ae
NazmulHasanShefat <mdnazmulhasan0879@gmail.com>
2026-05-24 13:31:08 +0600

    added loding animation in search input

--- src/app/all-pets/SearchInput.jsx
+++ src/app/all-pets/SearchInput.jsx
@@ -1,12 +1,15 @@
 "use client";
 import { SearchField } from "@heroui/react";
-import { useRouter, useSearchParams } from "next/navigation";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
+import { TbFidgetSpinnerFilled } from "react-icons/tb";
 
 export default function SearchInput() {
   const [text, setText] = useState(null);
+  const [isLoding, setLoding] = useState(false);
   const router = useRouter();
   const searchParamsText = useSearchParams();
+  const pathname = usePathname();
 
   // const handleSearch = (value) => {
   //   setText(value);
@@ -21,38 +24,51 @@ export default function SearchInput() {
   //   }
   // };
 
-
-    useEffect(() => {
+  useEffect(() => {
     const delayDebounce = setTimeout(() => {
       const params = new URLSearchParams(searchParamsText);
-
       if (text === "" || !text) {
         router.push("/all-pets");
         return;
       }
-
       params.set("SearchText", text);
       router.push(`/all-pets?${params.toString()}`);
+      setLoding(true);
+      return () => clearTimeout(delayDebounce);
     }, 500);
 
     return () => clearTimeout(delayDebounce);
   }, [text]);
 
+  useEffect(() => {
+    setLoding(false); // pathname বা searchParams বদলালে loading false
+  }, [pathname, searchParamsText]);
 
   return (
-    <SearchField
-      value={text}
-      // onChange={(value) => {
-      //   handleSearch(value);
-      // }}
-       onChange={(value) => setText(value)}
-      className={`border border-gray-400 rounded-xl`}
-    >
-      <SearchField.Group>
-        <SearchField.SearchIcon />
-        <SearchField.Input placeholder="Search..." />
-        <SearchField.ClearButton />
-      </SearchField.Group>
-    </SearchField>
+    <>
+      {isLoding ? (
+        <div className="fixed left-0 top-0 text-emerald-700 z-50 h-screen w-full bg-black/40 flex items-center justify-center">
+          <TbFidgetSpinnerFilled size={120} className="animate-spin" />
+        </div>
+      ) : (
+        ""
+      )}
+      <SearchField
+        value={text}
+        // onChange={(value) => {
+        //   handleSearch(value);
+        // }}
+        onChange={(value) => {
+          setText(value);
+        }}
+        className={`border border-gray-400 rounded-xl`}
+      >
+        <SearchField.Group>
+          <SearchField.SearchIcon />
+          <SearchField.Input placeholder="Search..." />
+          <SearchField.ClearButton />
+        </SearchField.Group>
+      </SearchField>
+    </>
   );
 }
--- src/app/all-pets/page.jsx
+++ src/app/all-pets/page.jsx
@@ -12,71 +12,84 @@ export const metadata = {
 
 export default async function Mypage({ searchParams }) {
   const params = await searchParams;
-   const selectedKey =  params?.filter;
-   console.log(selectedKey, "this is key")
+  const selectedKey = params?.filter;
+  console.log(selectedKey, "this is key");
 
-     const SearchText = params?.SearchText;
-   
-     const url = SearchText
-       ? `${baseURL}/search?query=${SearchText}`
-       : selectedKey && selectedKey !== "All"
-         ? `${baseURL}/filter-pets?species=${selectedKey}`
-         : `${baseURL}/all-pets`;
+  const SearchText = params?.SearchText;
 
+  // const url = SearchText
+  //   ? `${baseURL}/search?query=${SearchText}`
+  //   : selectedKey && selectedKey !== "All"
+  //     ? `${baseURL}/filter-pets?species=${selectedKey}`
+  //     : `${baseURL}/all-pets`;
 
-     const allPetsPromise = fetch(url)
-       .then((res) => res.json())
-       .catch((error) => console.log(error));
-  
-   
-     const spiciesCategoryPromise = fetch(`${baseURL}/find-uniqueCategorys`)
-       .then((res) => res.json())
-       .catch((error) => console.log(error));
+  const getPetsPromise = () => {
+    if (SearchText) {
+      const promise = fetch(`${baseURL}/search?query=${SearchText}`)
+        .then((res) => res.json())
+        .catch((error) => console.log(error));
+      return promise;
+    }
+    if (selectedKey && selectedKey !== "All") {
+      const promise = fetch(`${baseURL}/filter-pets?species=${selectedKey}`)
+        .then((res) => res.json())
+        .catch((error) => console.log(error));
+      return promise;
+    } else {
+      const promise = fetch(`${baseURL}/all-pets`)
+        .then((res) => res.json())
+        .catch((error) => console.log(error));
+      return promise;
+    }
+  };
+  const allPetsPromise = getPetsPromise();
+
+  const spiciesCategoryPromise = fetch(`${baseURL}/find-uniqueCategorys`)
+    .then((res) => res.json())
+    .catch((error) => console.log(error));
 
   return (
     <section className="w-full max-w-7xl mx-auto px-5 mt-5">
-       <div>
-          <div className="grid grid-cols-1 md:grid-cols-6">
-            <div className="md:col-span-3">
-              <h1 className="text-2xl md:text-3xl font-bold">
-                Meet Our Adorable Pets
-              </h1>
-              <p className="text-gray-500">
-                Explore verified pet listings and connect with pets looking for a
-                second chance at happiness.
-              </p>
+      <div>
+        <div className="grid grid-cols-1 md:grid-cols-6">
+          <div className="md:col-span-3">
+            <h1 className="text-2xl md:text-3xl font-bold">
+              Meet Our Adorable Pets
+            </h1>
+            <p className="text-gray-500">
+              Explore verified pet listings and connect with pets looking for a
+              second chance at happiness.
+            </p>
+          </div>
+          <div className="md:col-span-3 flex flex-col gap-5 md:flex-row items-center justify-between">
+            <div className="searchbox w-full md:w-1/2 mt-5 md:mt-0">
+              <SearchInput
+                SearchText={SearchText}
+                // setSearchText={setSearchText}
+              />
             </div>
-            <div className="md:col-span-3 flex flex-col gap-5 md:flex-row items-center justify-between">
-              <div className="searchbox w-full md:w-1/2 mt-5 md:mt-0">
-                <SearchInput
-                  SearchText={SearchText}
-                  // setSearchText={setSearchText}
+            <div className="filter-box w-full md:w-1/2">
+              <Suspense
+                fallback={
+                  <select className="w-full md:mb-0 mb-3 dark:bg-gray-700 border border-emerald-700 py-2 px-4 rounded-xl">
+                    <option value="loding...">loding....</option>
+                  </select>
+                }
+              >
+                <FilterInput
+                  spiciesCategoryPromise={spiciesCategoryPromise}
+                  setSelectedKey={selectedKey}
                 />
-              </div>
-              <div className="filter-box w-full md:w-1/2">
-                <Suspense
-                  fallback={
-                    <select className="w-full md:mb-0 mb-3 dark:bg-gray-700 border border-emerald-700 py-2 px-4 rounded-xl">
-                      <option value="loding...">loding....</option>
-                    </select>
-                  }
-                >
-                  <FilterInput
-                    spiciesCategoryPromise={spiciesCategoryPromise}
-                    setSelectedKey={selectedKey}
-                  />
-                </Suspense>
-              </div>
+              </Suspense>
             </div>
           </div>
-          <Suspense fallback={<SkeletonCard />}>
-            <div className="grid grid-cols-1 mt-5 md:grid-cols-2 lg:grid-cols-4 gap-7">
-              <PetCardAll allPetsPromise={allPetsPromise} />
-            </div>
-          </Suspense>
         </div>
+        <Suspense fallback={<SkeletonCard />}>
+          <div className="grid grid-cols-1 mt-5 md:grid-cols-2 lg:grid-cols-4 gap-7">
+            <PetCardAll allPetsPromise={allPetsPromise} />
+          </div>
+        </Suspense>
+      </div>
     </section>
   );
 }
-
-
