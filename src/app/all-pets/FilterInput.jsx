2509891ae890981a797c544e3c4574e83316b06b
NazmulHasanShefat <mdnazmulhasan0879@gmail.com>
2026-05-24 14:13:50 +0600

    added loading in filter

--- src/app/all-pets/FilterInput.jsx
+++ src/app/all-pets/FilterInput.jsx
@@ -1,15 +1,19 @@
 "use client";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { use, useState } from "react";
+import { use, useEffect, useState } from "react";
+import { TbFidgetSpinnerFilled } from "react-icons/tb";
 
 export function FilterInput({ spiciesCategoryPromise }) {
-  const [filterVal, setFilterVal] = useState(null);
+  const [isLoading, setLoading] = useState(false);
   const router = useRouter();
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const getPromise = use(spiciesCategoryPromise);
   const abilableSpicies = getPromise?.UniqueSpicies;
   console.log(abilableSpicies);
+
+  const a = "apple"
+  
 
   const handleChange = (e) => {
     if (e.target.value === "All") {
@@ -18,25 +22,39 @@ export function FilterInput({ spiciesCategoryPromise }) {
       const urlParams = new URLSearchParams(searchParams);
       urlParams.set("filter", e.target.value);
       router.push(`${pathname}?${urlParams.toString()}`);
+      setLoading(true);
     }
   };
 
+  useEffect(() => {
+    setLoading(false);
+  }, [pathname, searchParams]);
+
   return (
-    <select
-      onChange={handleChange}
-      aria-labelledby="section-title"
-      className="w-full dark:bg-gray-700 md:mb-0 mb-3 border border-emerald-700 py-2 px-4 rounded-xl"
-    >
-      <option value={`All`}>all</option>
-      {abilableSpicies &&
-        abilableSpicies.map((option) => {
-          return (
-            <option key={option?.id} value={option?.name}>
-              {" "}
-              {option.name}{" "}
-            </option>
-          );
-        })}
-    </select>
+    <>
+      {isLoading ? (
+        <div className="fixed left-0 top-0 text-emerald-700 z-50 h-screen w-full bg-black/40 flex items-center justify-center">
+          <TbFidgetSpinnerFilled size={120} className="animate-spin" />
+        </div>
+      ) : (
+        ""
+      )}
+      <select
+        onChange={handleChange}
+        aria-labelledby="section-title"
+        className="w-full dark:bg-gray-700 md:mb-0 mb-3 border border-emerald-700 py-2 px-4 rounded-xl"
+      >
+        <option value={`All`}>all</option>
+        {abilableSpicies &&
+          abilableSpicies.map((option) => {
+            return (
+              <option key={option?.id} value={option?.name}>
+                {" "}
+                {option.name}{" "}
+              </option>
+            );
+          })}
+      </select>
+    </>
   );
 }
